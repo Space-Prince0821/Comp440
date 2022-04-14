8d1ccc721fce6c8d7fe2645f9441260a7a244ede
mpaul97 <85245587+mpaul97@users.noreply.github.com>
2022-04-13 20:02:04 -0700

    add some more fk's to tables/new hobby table to store available hobbies

--- Initdb.sql
+++ Initdb.sql
@@ -11,7 +11,6 @@ CREATE TABLE user (
     `firstName` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
     `lastName` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
     `email` varchar(100) COLLATE utf8mb4_general_ci UNIQUE
-    -- PRIMARY KEY (`username`)
 );
 
 -- Add 5 users
@@ -25,31 +24,54 @@ INSERT INTO `user` (username, password, firstName, lastName, email)  VALUES
 -- Create hobby table
 DROP TABLE IF EXISTS `hobbies`;
 CREATE TABLE hobbies (
-    `hobby_id` INT COLLATE utf8mb4_general_ci NOT NULL,
-    `user_id` INT COLLATE utf8mb4_general_ci NOT NULL
+    `hobby_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `hobby_name` varchar(20) COLLATE utf8mb4_general_ci NOT NULL
 );
 
-INSERT INTO `hobbies` VALUES
-    (1, 4),
-    (1, 6),
-    (3, 4),
-    (2, 5),
-    (4, 1);
+-- All hobbies: hiking, swimming, calligraphy, bowling, movie, cooking, and dancing
+
+INSERT INTO `hobbies` (hobby_name) VALUES
+    ('Hiking'),
+    ('Swimming'),
+    ('Calligraphy'),
+    ('Bowling'),
+    ('Movie'),
+    ('Cooking'),
+    ('Dancing');
+
+-- Create user's hobbies table
+DROP TABLE IF EXISTS `UserHobbies`;
+CREATE TABLE `UserHobbies` (
+    `hobby_id` INT DEFAULT NULL,
+    CONSTRAINT `hobby_id_fk` FOREIGN KEY (`hobby_id`) REFERENCES `hobbies` (`hobby_id`) ON DELETE SET NULL,
+    `user_id` INT DEFAULT NULL,
+    CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE SET NULL
+);
+
+INSERT INTO `UserHobbies` VALUES
+    (1, 1),
+    (1, 5),
+    (2, 2),
+    (3, 3),
+    (5, 7);
 
 -- Create blog table
 DROP TABLE IF EXISTS `blog`;
 CREATE TABLE blog (
     `blog_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `user_id` INT DEFAULT NULL,
+    CONSTRAINT `user_id_fk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE SET NULL,
+    `date` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
     `subject` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-    `description` varchar(200) COLLATE utf8mb4_general_ci NOT NULL
+    `description` varchar(1000) COLLATE utf8mb4_general_ci NOT NULL
 );
 
-INSERT INTO `blog` (subject, description) VALUES
-    ('soccer', 'dhfshdfhsdh  dfsdfds'),
-    ('bitcoin', 'djdfsjdfj dfs dfs'),
-    ('soccer', 'dhfshdfhsdh  dfsdfds'),
-    ('bitcoin', 'djdfsjdfj dfs dfs'),
-    ('swimmming', 'ddfhds dj dsdf fjdjfdjjfs sdfjdfjsjd fhdsf dsdjf');
+INSERT INTO `blog` (user_id, date, subject, description) VALUES
+    (1, CURDATE(), 'Soccer', "Association football, more commonly known as simply football or soccer,[a] is a team sport played with a spherical ball between two teams of 11 players. It is played by approximately 250 million players in over 200 countries and dependencies, making it the world's most popular sport.");
+    -- ('bitcoin', 'djdfsjdfj dfs dfs'),
+    -- ('soccer', 'dhfshdfhsdh  dfsdfds'),
+    -- ('bitcoin', 'djdfsjdfj dfs dfs'),
+    -- ('swimmming', 'ddfhds dj dsdf fjdjfdjjfs sdfjdfjsjd fhdsf dsdjf');
 
 -- Create tags table
 DROP TABLE IF EXISTS `tags`;
@@ -72,7 +94,6 @@ CREATE TABLE comment (
     `sentiment` BIT COLLATE utf8mb4_general_ci NOT NULL,
     `description` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
     `blog_id` INT DEFAULT NULL,
-    KEY `blog_id` (`blog_id`),
     CONSTRAINT `blog_id_fk` FOREIGN KEY (`blog_id`) REFERENCES `blog` (`blog_id`) ON DELETE SET NULL
 );
 
