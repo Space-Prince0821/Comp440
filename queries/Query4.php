f918f837e00c9977b39cab56171e0faab84ea49f
Prince Chowdury <78129627+Space-Prince0821@users.noreply.github.com>
2022-04-29 21:55:37 -0700

    Revert "query 2"
    This reverts commit c18d860832e306751a860a18987bddb281f16ef0, reversing
    changes made to 36e30c0dc3d225a46f760b14a267386086c20815.

--- queries/Query4.php
+++ queries/Query4.php
@@ -1,7 +1,4 @@
 <html>
-    <?php
-        include("../Config.php");
-    ?>
     <head>
         <meta charset="utf-8">
         <title>Comment</title>
@@ -25,67 +22,9 @@
         </style>
     </head>
     <body>
-    <div class="container">
+        <div class="container">
+            <button>Run Query 4</button>
             <br>
-            <h3 style="color: white;">Query 4: Users who are followed by user X  and user Y</h3>
-            <br>
-
-            <form name="form" action="" method="get">
-                <input type="text" name="userX" id="userX" value="">
-                <input type="text" name="userY" id="userY" value="">
-                <br>
-                <input type="submit" name="submit" />
-            
-
-            <?php 
-                // $userX = $_REQUEST['userX'];
-                // $userY = $_REQUEST['userY'];
-                // $id_X='';
-                // $id_Y='';
-
-                // $getUlist = "SELECT * FROM users 
-                // WHERE username=$userX AND username=$userY";
-                // $uResult = mysqli_query($db, $getUlist);
-            
-                if(isset($_POST['submit']) && isset($_GET["userX"]) && 
-                         $_GET["userX"] != "" && isset($_GET["userY"]) && 
-                         $_GET["userY"] != "") {
-                    echo 'works';
-
-                    $userX = $_REQUEST['userX'];
-                    $userY = $_REQUEST['userY'];
-                    $id_X='';
-                    $id_Y='';
-
-                    $getUlist = "SELECT * FROM users 
-                    WHERE username=$userX AND username=$userY";
-                    $uResult = mysqli_query($db, $getUlist);
-                    
-                    foreach($uResult as $uResultRow) {
-                        $temp_id = $uResultRow['user_id'];
-                        if($uResultRow['username'] == $userX) {
-                            $id_X = $uResultRow['user_id'];
-                        }
-                        if($uResultRow['username'] == $userY) {
-                            $id_Y = $uResultRow['user_id'];
-                        }
-
-                        $sql = "SELECT * FROM follow GROUP BY user_id 
-                        HAVING COUNT(user_id) > 1 
-                        AND follows_user_id=$id_X OR follows_user_id=$id_Y";
-                        $result = mysqli_query($db, $sql);
-    
-                        foreach($result as $resultRow) {
-                            echo $userX . ' and ' . $userY . ' follow: ' . 
-                            '<button>' . $resultRow['username'] . '</button>';
-                        }
-                        
-                    }
-                }
-
-                
-            ?>
-            </form>
             <a href="../welcome.php">
                 <button>Return to Home</button>
             </a>
