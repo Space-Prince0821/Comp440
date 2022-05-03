6f03e1e7bf7d575bb4b3354ac8c6afede4ed2970
Prince Chowdury <78129627+Space-Prince0821@users.noreply.github.com>
2022-05-03 15:25:03 -0700

    Finished query 1

--- queries/Query1.php
+++ queries/Query1.php
@@ -79,6 +79,10 @@
                 include("../Config.php");
 
                 //These are the tags stores in variables once the user chooses from dropdown
+                $tempArrForBlogId = array();
+                $tempArrForUserId = array();
+                $tempArrForAllUserIds = array();
+
                 $selTag1Query = $_REQUEST["tagForm"]; 
                 $selTag2Query = $_REQUEST["tagForm2"];
                 echo '<p> Tag 1 Name: ' . $selTag1Query . '</p>';   
@@ -95,7 +99,38 @@
                 echo '<p> Tag 1 ID: ' . $tagID1 . '</p>';
                 echo '<p> Tag 2 ID: ' . $tagID2 . '</p>';
 
-                //Then I dont know what to do...
+                //Putting all blog IDs that use the two tags in an array
+                $blog_query = "SELECT blog_id FROM BlogTags WHERE tag_id = '$tagID1'";
+                $blog_res = mysqli_query($db, $blog_query);
+                foreach($blog_res as $a){
+                    $id = $a['blog_id'];
+                    array_push($tempArrForBlogId, $id);
+                }
+                $blog_query2 = "SELECT blog_id FROM BlogTags WHERE tag_id = '$tagID2'";
+                $blog_res2 = mysqli_query($db, $blog_query2);
+                foreach($blog_res2 as $b){
+                    $id2 = $b['blog_id'];
+                    array_push($tempArrForBlogId, $id2);
+                }
+
+                //Putting all user_ids using tags
+                foreach($tempArrForBlogId as $c) {
+                    $userID_query = "SELECT distinct user_id FROM blog WHERE blog_id = '$c'";
+                    $userID_res = mysqli_query($db, $userID_query);
+                    $curr_res = $userID_res->fetch_array()[0] ?? '';
+                    array_push($tempArrForUserId, $curr_res); 
+                }
+                $tempArrForAllUserIds = array_unique($tempArrForUserId);
+
+                //echo $tempArrForUserId[3];
+                foreach($tempArrForAllUserIds as $d) {
+                    if (count(array_keys($tempArrForUserId, $d)) > 1) {
+                        $finalQ = "SELECT username FROM users WHERE user_id = '$d'";
+                        $finalRes = mysqli_query($db, $finalQ);
+                        $finalUserName = $finalRes->fetch_array()[0] ?? '';
+                        echo '<p>' . $finalUserName . '</p>';
+                    }
+                }
             ?>
             <a href="../welcome.php">
                 <button>Return to Home</button>
